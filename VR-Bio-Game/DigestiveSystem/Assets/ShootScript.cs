f534634fde5193162439154f386cd406bd48612e
ahmoody2000 <58375227+ahmedmoh1234@users.noreply.github.com>
2021-08-17 01:45:49 +0200

    Added food assets

--- VR-Bio-Game/DigestiveSystem/Assets/ShootScript.cs
+++ VR-Bio-Game/DigestiveSystem/Assets/ShootScript.cs
@@ -11,19 +11,24 @@ public class ShootScript : MonoBehaviour
     private float _y = 0f;
     private float _x = 0f;
 
-    private Vector3 _offset = new Vector3(-1f, 0f, 0f);
+    private Vector3 _fireOffset = new Vector3(-1f, 0f, 0f);
     private const int _bulletSpeed = 150;
 
     public Canvas can;
 
+    public GameObject gun;
+    private Vector3 _gunOffset = new Vector3(0f,-0.75f,0f);
+
     private short _bulletType = 1;
     //1 --> Pepsin
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+
     }
 
     // Update is called once per frame
@@ -34,9 +39,9 @@ public class ShootScript : MonoBehaviour
             //Primary key
             try
             {
-                _offset = this.transform.position + 1.5f * this.transform.forward;
-                _gObjList.Add(Instantiate(Bullet, _offset, Quaternion.LookRotation(this.transform.forward, Vector3.up)));
-                _gObjList[_gObjList.Count - 1].gameObject.GetComponent<Rigidbody>().AddForce(_bulletSpeed * this.transform.forward);
+                _fireOffset = this.transform.position + 1.5f * this.transform.forward;
+                //_gObjList.Add(Instantiate(Bullet, _fireOffset, Quaternion.LookRotation(this.transform.forward, Vector3.up)));
+                //_gObjList[_gObjList.Count - 1].gameObject.GetComponent<Rigidbody>().AddForce(_bulletSpeed * this.transform.forward);
             }
             catch
             {
@@ -48,5 +53,8 @@ public class ShootScript : MonoBehaviour
         _y += _mouseSpeed * Input.GetAxis("Mouse X");
 
         this.transform.eulerAngles = new Vector3(_x, _y, 0f);
+
+        gun.gameObject.transform.position = this.gameObject.transform.position + _gunOffset;//+ this.gameObject.transform.forward.normalized;
+        gun.gameObject.transform.forward = this.gameObject.transform.forward;
     }
 }
