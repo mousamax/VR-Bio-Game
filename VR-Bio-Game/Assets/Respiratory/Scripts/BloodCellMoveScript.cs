9b9e76e13c2d27e26b7c2df9d029547a62143e51
NaderYouhanna <naderyouhanna@gmail.com>
2021-08-23 09:02:41 +0200

    Changed Spawning paameters (increased speed and decreasd difference time between spawns)

--- VR-Bio-Game/Assets/Respiratory/Scripts/BloodCellMoveScript.cs
+++ VR-Bio-Game/Assets/Respiratory/Scripts/BloodCellMoveScript.cs
@@ -11,7 +11,7 @@ public class BloodCellMoveScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        this.gameObject.transform.position += (this.gameObject.transform.forward.normalized / 50f);
+        this.gameObject.transform.position += (this.gameObject.transform.forward.normalized / 20f);
         if ( this.gameObject.transform.position.z >= MaxZ )
         {
             Destroy(this.gameObject);
