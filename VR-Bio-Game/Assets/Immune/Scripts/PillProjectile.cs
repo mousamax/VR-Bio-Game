85da3da786027289367e36c4074b2439182e1796
nadaabdelgayed <nada.ahmed15@stemmaadi.edu.eg>
2021-08-23 08:20:48 +0200

    improving and attaching scripts to game objects
    pill collision with external is not working in Pill Projectile script.

--- VR-Bio-Game/Assets/Immune/Scripts/PillProjectile.cs
+++ VR-Bio-Game/Assets/Immune/Scripts/PillProjectile.cs
@@ -18,44 +18,43 @@ public class PillProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-  
         //Press A to throw the pill farward and activate the gravity
-        if (gameObject.GetComponent<Weapons>().isSelected()==true && Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if (gameObject.GetComponent<Weapons>().isSelected()==true )
         {
             Debug.Log("A is pressed");
             rigidbody.AddForce(0, m_Thurst, m_Thurst * 3, ForceMode.Impulse);
             rigidbody.useGravity = true;
             Debug.Log("Pill is Projected");
         }
+        }
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject GO = collision.gameObject;
-        if (GO.tag == "External" || GO.tag == "Slime" || GO.tag == "Spike" || GO.tag == "FatBlob" || GO.tag == "RedCell")
+        Debug.Log("pill collided with: "+collision.gameObject.tag);
+        Debug.Log("Pill is collided");
+        GameObject explosion;
+        for (int i = 0; i < 5; i++)
         {
-            Debug.Log("Pill is collided");
-            GameObject explosion;
-            for (int i = 0; i < 5; i++)
+            explosion = explosionEffect.transform.GetChild(i).gameObject;
+            if (!explosion.activeSelf)
             {
-                explosion = explosionEffect.transform.GetChild(i).gameObject;
-                if (!explosion.activeSelf)
-                {
-                    Vector3 position = transform.position;
-                    Quaternion rotation = transform.rotation;
-
-                    explosion.SetActive(true);
-                    explosion.transform.position = position;
-                    explosion.transform.rotation = rotation;
-                    Debug.Log("Pill is exploded");
-                    break;
-                }
+                Vector3 position = transform.position;
+                Quaternion rotation = transform.rotation;
+                explosion.SetActive(true);
+                explosion.transform.position = position;
+                explosion.transform.rotation = rotation;
+                Debug.Log("Pill is exploded");
+                break;
             }
-            this.transform.position = new Vector3(-70, -70, -70);
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.angularVelocity = Vector3.zero;
-            this.gameObject.SetActive(false);
         }
-
+        this.transform.position = new Vector3(-70, -70, -70);
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        Debug.Log("Pill will disappear");
+        this.gameObject.SetActive(false);
     }
 
 }
