58abae30bb087fc75db684f8ff58a277809662d8
NaderYouhanna <naderyouhanna@gmail.com>
2021-08-24 14:21:11 +0200

    Update BloodCellMoveScript.cs

--- VR-Bio-Game/Assets/Respiratory/Scripts/BloodCellMoveScript.cs
+++ VR-Bio-Game/Assets/Respiratory/Scripts/BloodCellMoveScript.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class BloodCellMoveScript : MonoBehaviour
 {
 
-    public float MaxZ;
 
 
     // Update is called once per frame
     private void Update()
     {
         this.gameObject.transform.position += (this.gameObject.transform.forward.normalized / 20f);
-        if ( this.gameObject.transform.position.z >= MaxZ )
-        {
-            Destroy(this.gameObject);
-        }
+       
     }
 
    
