ebcedcb8feb1c4279b8ac4c05f996eb1b6008d4c
Moaz Elsherbini <moaaz.elsherbini00@eng-st.cu.edu.eg>
2021-09-03 00:36:05 +0200

    Auto stash before merge of "DigestiveSystem" and "origin/DigestiveSystem"

--- VR-Bio-Game/DigestiveSystem/Assets/Custom Weapons/HammerCollider.cs
+++ VR-Bio-Game/DigestiveSystem/Assets/Custom Weapons/HammerCollider.cs
@@ -6,10 +6,30 @@ public class HammerCollider : MonoBehaviour
 {
     private int _noOfFoodComponents = 2;
     public Vector3 FoodComponentScale;
-    private void OnTriggerEnter(Collider collision)
+    //private void OnTriggerEnter(Collider collision)
+    //{
+    //    Debug.Log(collision.transform.tag);
+    //    if(collision.transform.gameObject != null && collision.gameObject.CompareTag("Food"))
+    //    {
+
+    //        for (int i = 0; i < _noOfFoodComponents; i++)
+    //        {
+    //            createCube(collision);
+    //        }
+
+    //        //Debug.Log("food = " + collision.gameObject.name);
+    //        //if(collision.GetComponent<Move_food>() != null)
+    //        Destroy(collision.gameObject);
+
+    //        //collision.GetComponent<Move_food>().isDestroy = true;
+    //            //collision.gameObject.SetActive(false);
+         
+    //    }
+    //}
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.transform.tag);
-        if(collision.transform.gameObject != null && collision.gameObject.CompareTag("Food"))
+        //Debug.Log(collision.transform.tag);
+        if (collision.transform.gameObject != null && collision.gameObject.CompareTag("Food"))
         {
 
             for (int i = 0; i < _noOfFoodComponents; i++)
@@ -19,22 +39,21 @@ public class HammerCollider : MonoBehaviour
 
             //Debug.Log("food = " + collision.gameObject.name);
             //if(collision.GetComponent<Move_food>() != null)
-            //Destroy(collision.gameObject);
+            Destroy(collision.gameObject);
 
             //collision.GetComponent<Move_food>().isDestroy = true;
-                //collision.gameObject.SetActive(false);
-         
+            //collision.gameObject.SetActive(false);
+
         }
     }
 
-    
 
-    private void createCube(Collider collision)
+    private void createCube(Collision collision)
     {
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube.transform.position = collision.transform.position;
         cube.transform.localScale = FoodComponentScale;
-        //cube.AddComponent<Rigidbody>();
+        cube.AddComponent<Rigidbody>();
         cube.AddComponent<BoxCollider>();
     }
 }
