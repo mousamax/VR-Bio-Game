16bf5427d914eb30b870cddcfbe6894fdfa5949b
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2021-09-04 01:59:42 +0200

    modify Events and gamemanagers
    Immune system

--- VR-Bio-Game/Assets/Immune/Scripts/WeaponChange.cs
+++ VR-Bio-Game/Assets/Immune/Scripts/WeaponChange.cs
@@ -11,11 +11,12 @@ public class WeaponChange : MonoBehaviour
     public Transform SwordPlace;
     public Transform GunPlace;
     public Transform PillPlace;
-    Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-      ResetPosition();
+        PillResetPosition();
+        SwordResetPosition();
+        GunResetPosition();
     }
 
     // Update is called once per frame
@@ -24,54 +25,74 @@ public class WeaponChange : MonoBehaviour
         // Press P to summon the pill at the weapon place
         if (Input.GetKeyDown(KeyCode.P))
         {
-            ResetPosition();
-            GameObject pill;
-            for (int i = 0; i < 5; i++)
-            {
-                pill = Pill.transform.GetChild(i).gameObject;
-                if (!pill.activeSelf)
-                {
-                    pill.SetActive(true);
-                    pill.transform.position = weaponPlace.position;
-                    pill.transform.rotation = weaponPlace.rotation;
-                    pill.GetComponent<Weapons>().Select(true);
-                    // Debug.Log("pill is in place");
-                    break;
-                }
-            }
+            GunResetPosition();
+            SwordResetPosition();
+            Pill.transform.position = weaponPlace.position;
+            Pill.transform.rotation = weaponPlace.rotation;
+            Pill.GetComponent<Weapons>().Select(true);
+
         }
         else if (Input.GetKeyDown(KeyCode.G))
         {
-            ResetPosition();
+            PillResetPosition();
+            SwordResetPosition();
             Gun.transform.position = weaponPlace.position;
-            Gun.transform.rotation = weaponPlace.rotation * Quaternion.Euler(0,180,0);
+            Gun.transform.rotation = weaponPlace.rotation * Quaternion.Euler(0, 180, 0);
             Gun.GetComponent<Weapons>().Select(true);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            ResetPosition();
+            PillResetPosition();
+            GunResetPosition();
             Sword.transform.position = weaponPlace.position;
             Sword.transform.rotation = weaponPlace.rotation;
             Sword.GetComponent<Weapons>().Select(true);
         }
+        //FOR VR PLAYING MODE
+        if (Pill.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            GunResetPosition();
+            SwordResetPosition();
+            Pill.GetComponent<Weapons>().Select(true);
+            Pill.GetComponent<Rigidbody>().useGravity = true;
+        }
+        else if (Gun.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            PillResetPosition();
+            SwordResetPosition();
+            Gun.GetComponent<Weapons>().Select(true);
+            Gun.GetComponent<Rigidbody>().useGravity = true;
+
+        }
+        else if (Sword.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            PillResetPosition();
+            GunResetPosition();
+            Sword.GetComponent<Weapons>().Select(true);
+            Sword.GetComponent<Rigidbody>().useGravity = true;
+
+        }
     }
-    private void ResetPosition()
+
+    private void PillResetPosition()
+    {
+        Pill.transform.position = PillPlace.position;
+        Pill.GetComponent<Weapons>().Select(false);
+        Pill.GetComponent<Rigidbody>().useGravity = false;
+    }
+    private void SwordResetPosition()
+    {
+        Sword.transform.position = SwordPlace.position;
+        Sword.GetComponent<Weapons>().Select(false);
+        Sword.GetComponent<Rigidbody>().useGravity = false;
+
+
+    }
+    private void GunResetPosition()
     {
         Gun.transform.position = GunPlace.position;
-        Sword.transform.position = SwordPlace.position;
         Gun.GetComponent<Weapons>().Select(false);
-        Sword.GetComponent<Weapons>().Select(false);
-        GameObject pill;
-        for (int i = 0; i < 5; i++)
-        {
-            pill=Pill.transform.GetChild(i).gameObject;
-            if(pill.activeSelf== false)
-            {
-                rigidbody=pill.GetComponent<Rigidbody>();
-                rigidbody.useGravity = false;
-                pill.transform.position = PillPlace.position;
-                pill.GetComponent<Weapons>().Select(true);
-            }
-        }
+        Gun.GetComponent<Rigidbody>().useGravity = false;
+
     }
 }
