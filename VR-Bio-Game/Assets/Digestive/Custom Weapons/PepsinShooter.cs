fe2d8202e0ef33660d4d4ca85a8f37201a54df4b
ahmoody2000 <58375227+ahmedmoh1234@users.noreply.github.com>
2021-09-13 22:11:17 +0200

    Fixed the weapon and ammo activation

--- VR-Bio-Game/Assets/Digestive/Custom Weapons/PepsinShooter.cs
+++ VR-Bio-Game/Assets/Digestive/Custom Weapons/PepsinShooter.cs
@@ -11,7 +11,7 @@ namespace DigestiveSystem
         public GameObject bulletParent;
         public GameObject nozzle;
         public bool CanShoot = true;
-                
+
         private int _maxBullets;
         public int MagazineIndex = 1;
         //private static int _bulletIndex = 0;
@@ -30,52 +30,51 @@ namespace DigestiveSystem
 
         void Update()
         {
-            if (Input.GetMouseButtonDown(0) || (this.GetComponent<OVRGrabbable>().isGrabbed && OVRInput.Get(OVRInput.RawButton.RIndexTrigger)))
+            if (CanShoot && Input.GetMouseButtonDown(0) || (this.GetComponent<OVRGrabbable>().isGrabbed && OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)))
             {
                 Shoot();
             }
-            if (!CanShoot && (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Two)) )
+            if (!CanShoot && (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Two)))
             {
                 this.gameObject.transform.GetChild(MagazineIndex).GetComponent<Rigidbody>().isKinematic = false;
             }
 
-            
+
         }
 
         private void Shoot()
         {
 
             GameObject bullet;
-            if (CanShoot)
+
+            for (int i = 0; i < _maxBullets; i++)
             {
-                for (int i = 0; i < _maxBullets; i++)
+                bullet = bulletParent.transform.GetChild(i).gameObject;
+                if (!bullet.activeSelf)
                 {
-                    bullet = bulletParent.transform.GetChild(i).gameObject;
-                    if (!bullet.activeSelf)
-                    {
-                        Vector3 position = nozzle.transform.position;
-                        Quaternion rotation = nozzle.transform.rotation;
-                        //rotation.eulerAngles.z += 
-                        //gunAudioSource.PlayOneShot(shootClip);
-                        //Rigidbody rb = bullet.GetComponent<Rigidbody>();
-                        //Vector3 m_EulerAngleVelocity = new Vector3(0, 0, 270);
-                        //Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
-                        //rb.MoveRotation(rb.rotation * deltaRotation);
-                        bullet.SetActive(true);
-                        bullet.transform.position = position;
-                        bullet.transform.rotation = rotation;
-                        bullet.transform.forward = this.transform.forward;
+                    Vector3 position = nozzle.transform.position;
+                    Quaternion rotation = nozzle.transform.rotation;
+                    //rotation.eulerAngles.z += 
+                    //gunAudioSource.PlayOneShot(shootClip);
+                    //Rigidbody rb = bullet.GetComponent<Rigidbody>();
+                    //Vector3 m_EulerAngleVelocity = new Vector3(0, 0, 270);
+                    //Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity);
+                    //rb.MoveRotation(rb.rotation * deltaRotation);
+                    bullet.transform.position = position;
+                    bullet.transform.rotation = rotation;
+                    bullet.transform.forward = this.transform.forward;
+                    bullet.SetActive(true);
 
-                        break;
-                    }
-                    if (i == _maxBullets - 1)
-                    {
-                        CanShoot = false;
-                    }
-
+                    break;
                 }
+                if (i == _maxBullets - 1)
+                {
+                    CanShoot = false;
+                }
+
             }
-            
+
+
         }
     }
 
--- VR-Bio-Game/Assets/Digestive/Scripts/AmmoActivation.cs
+++ VR-Bio-Game/Assets/Digestive/Scripts/AmmoActivation.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmmoActivation : MonoBehaviour
+namespace DigestiveSystem
 {
-    public Color ActivatedColor;
-    public Color DeactivatedColor;
-    public bool activated = false;
-
-    private void OnParticleCollision(GameObject other)
+    public class AmmoActivation : MonoBehaviour
     {
-        //Debug.Log("collide with hcl");
-        if (!activated)
+        public Color ActivatedColor;
+        public Color DeactivatedColor;
+        public bool activated = false;
+
+        private void OnParticleCollision(GameObject other)
         {
-            activated = true;
-            var m = this.GetComponent<Renderer>();
-            if (m == null)
-                Debug.Log("NULL!");
-            if (m != null)
-                m.material.color = ActivatedColor;
-            
+            Debug.Log("collide with hcl");
+            if (!activated)
+            {
+                activated = true;
+                var m = this.gameObject.transform.GetChild(0).GetComponent<Renderer>();
+                if (m == null)
+                    Debug.Log("NULL!");
+                if (m != null)
+                    m.material.color = ActivatedColor;
+
+            }
         }
     }
 }
